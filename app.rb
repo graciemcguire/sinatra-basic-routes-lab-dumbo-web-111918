0e5346783b0ef6ef2095b4faac430a6f9716f396
Gracie McGuire <m.graciemcguire@gmail.com>
2018-12-09 20:45:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' { "My name is Gracie" }
+
+  
+  get '/hometown' do 
+    "My hometown is Denver"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is 'All My Friends'"
+  end
 end
